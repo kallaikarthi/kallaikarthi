c8c30179bbc59a4ef81512f73ff317c022020562
kallaikarthi <spkarthik.thachur@gmail.com>
2017-07-27 13:08:34 +0530

    Add files via upload

---
+++ local/loc.py
@@ -0,0 +1,31 @@
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+# @app.route('/')
+@app.route('/webhook', methods=['GET', 'POST'])
+def index():
+    req = request.get_json(silent=True, force=True)
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    result = req.get("result")
+    parameters = result.get("parameters")
+    variety = parameters.get("pizza-cost")
+    print variety
+    return variety
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
